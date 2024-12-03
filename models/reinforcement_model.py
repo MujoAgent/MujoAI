0e85741c3f7b7eb2149f31a89d1b6dc63c4b86e7
NeoAIZ <maitran080842@gmail.com>
2024-12-03 03:04:04 -0700

    Update reinforcement_model.py

--- models/reinforcement_model.py
+++ models/reinforcement_model.py
@@ -1,1 +1,12 @@
+class ReinforcementModel:
+    def init(self, name):
+        self.name = name
+        self.rewards = 0
 
+    def update_rewards(self, value):
+        self.rewards += value
+        print(f"{self.name} rewards updated to: {self.rewards}")
+
+    def act(self, state):
+        print(f"{self.name} is acting based on state: {state}")
+        return "Action"
