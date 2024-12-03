8ca2756fe0e4cffa7c39dac7516eda7867ebf7ad
NeoAIZ <maitran080842@gmail.com>
2024-12-03 02:03:48 -0800

    Update knowledge_base.py

--- core/knowledge_base.py
+++ core/knowledge_base.py
@@ -1,1 +1,14 @@
+class KnowledgeBase:
+    def __init__(self):
+        self.knowledge = {}
+
+    def add_entry(self, topic, content):
+        self.knowledge[topic] = content
+        print(f"Knowledge added: {topic}")
+
+    def get_entry(self, topic):
+        return self.knowledge.get(topic, "No knowledge on this topic.")
+
+    def list_topics(self):
+        return list(self.knowledge.keys())
 
