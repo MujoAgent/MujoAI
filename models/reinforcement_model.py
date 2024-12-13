dc1ba329303855b57c378fcfc4436791b011955d
MujoAI <maitran080842@gmail.com>
2024-12-13 16:48:37 -0700

    Update reinforcement_model.py

--- models/reinforcement_model.py
+++ models/reinforcement_model.py
@@ -2,7 +2,7 @@ class ReinforcementModel:
     def init(self, name):
         self.name = name
         self.rewards = 0
-
+ 
     def update_rewards(self, value):
         self.rewards += value
         print(f"{self.name} rewards updated to: {self.rewards}")
